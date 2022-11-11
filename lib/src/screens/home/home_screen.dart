ed7b6ac6b2300545066d0bc8b45d56280e9a6add
Nehemias Munoz <nehemiasmunozm@gmail.com>
2022-11-11 17:20:33 -0300

    Disenio barra de busqueda

--- lib/src/screens/home/home_screen.dart
+++ lib/src/screens/home/home_screen.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(child: Text('Home Screen')),
+      body: SafeArea(
+        child: SizedBox(
+          width: size.width * 1,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Card(
+                child: Container(
+                  width: size.width * 0.9,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.menu_outlined),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 3,
+                        child: TextField(
+                          textAlign: TextAlign.center,
+                          maxLines: 1,
+                          decoration: InputDecoration(
+                              hintText: "Search from notes",
+                              border: InputBorder.none),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
